b8d35a53068317e6851644ad9ffec4060aedd2f9
Diene89 <diene_89@hotmail.com>
2022-07-19 16:46:57 -0300

    feat: Requisito 7 - modelo "Category"

---
+++ src/database/models/category.js
@@ -0,0 +1,21 @@
+const createCategory = (sequelize, DataTypes) => {
+    const Category = sequelize.define('Category', {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            autoIncrement: true,
+            primaryKey: true,
+          },
+          name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+          },
+    }, {
+      timestamps: false,
+    }
+    )
+
+    return Category;
+};
+
+module.exports = createCategory;
